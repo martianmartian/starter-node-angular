57814a7cf03f66f4dc8be6646d6b055197f88071
iamwave007 <iamwave007@hotmail.com>
2014-12-13 20:38:40 -0500

    finished tutorial

---
+++ public/js/controllers/MainCtrl.js
@@ -0,0 +1,5 @@
+// public/js/controllers/MainCtrl.js
+
+angular.module('MainCtrl',[]).controllers('MainController',function($scope){
+  $scope.tagline = 'To the moon and back!';
+})
---
+++ public/js/controllers/NerdCtrl.js
@@ -0,0 +1,5 @@
+// public/js/controllers/NerdCtrl.js
+
+angular.module('NerdCtrl',[]).controllers('MainController',function($scope){
+  $scope.tagline = 'Nothing beats a pocket protector!';
+})
